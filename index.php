13326ed0207cca56e90a395cd0a4efb8f400f4fc
murateshimov <57397667+murateshimov@users.noreply.github.com>
2021-03-25 23:42:12 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+  <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'>
+  
+<div class="container">
+  <div class="header">
+    <div class="full-name">
+      <span class="first-name">Murat</span> 
+      <span class="last-name">Eshimov</span>
+    </div>
+    <div class="contact-info">
+      <span class="email">Email: </span>
+      <span class="email-val">murat.eshimov@gmail.com</span>
+      <span class="separator"></span>
+      <span class="phone">Phone: </span>
+      <span class="phone-val">+996 700-701-702</span>
+    </div>
+    
+    <div class="about">
+      <span class="position">Software Developer </span>
+      <span class="desc">
+        I am a computer science student with more than 3 years of experience writing html, css, and js. I'm motivated, result-focused and seeking a successful team-oriented company with opportunity to grow. 
+      </span>
+    </div>
+  </div>
+   <div class="details">
+    <div class="section">
+      <div class="section__title">Experience</div>
+      <div class="section__list">
+        <div class="section__list-item">
+          <div class="left">
+            <div class="name">Standart Bank Limited</div>
+            <div class="addr">Bishkek, Kyrgyzstan</div>
+            <div class="duration">Jan 2018 - Jan 2022</div>
+          </div>
+          <div class="right">
+            <div class="name">Senior IT manager</div>
+            <div class="desc">Database</div>
+          </div>
+        </div>
+                <div class="section__list-item">
+          <div class="left">
+            <div class="name">City Bank</div>
+            <div class="addr">Greenwich Street, NY</div>
+            <div class="duration">Jan 2016 - Feb 2018</div>
+          </div>
+          <div class="right">
+            <div class="name">Junior Developer</div>
+            <div class="desc">Mobile Development, Citi Bank app </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    <div class="section">
+      <div class="section__title">Education</div>
+      <div class="section__list">
+        <div class="section__list-item">
+          <div class="left">
+            <div class="name">International Ala-Too University </div>
+            <div class="addr">Bishkek, Kyrgyzstan</div>
+            <div class="duration">Jan 2011 - Feb 2015</div>
+          </div>
+          <div class="right">
+            <div class="name">Computer Science</div>
+            <div class="desc">Python, SQL, Web Development</div>
+          </div>
+        </div>
+      </div>
+      
+  </div>
+     <div class="section">
+      <div class="section__title">Projects</div> 
+       <div class="section__list">
+         <div class="section__list-item">
+           <div class="name">Heroku</div>
+           <div class="text">Created simple CV using HTML and CSS at Mr.Nurislam's lesson</div>
+         </div>
+         
+        
+       </div>
+    </div>
+     <div class="section">
+       <div class="section__title">Skills</div>
+       <div class="skills">
+         <div class="skills__item">
+           <div class="left"><div class="name">
+             Javascript
+             </div></div>
+           <div class="right">
+                          <input  id="ck1" type="checkbox" checked/>
+
+             <label for="ck1"></label>
+                          <input id="ck2" type="checkbox" checked/>
+
+              <label for="ck2"></label>
+                         <input id="ck3" type="checkbox" />
+
+              <label for="ck3"></label>
+                           <input id="ck4" type="checkbox" />
+            <label for="ck4"></label>
+                          <input id="ck5" type="checkbox" />
+              <label for="ck5"></label>
+
+           </div>
+         </div>
+         
+       </div>
+       <div class="skills__item">
+           <div class="left"><div class="name">
+             CSS</div></div>
+           <div class="right">
+                          <input  id="ck1" type="checkbox" checked/>
+
+             <label for="ck1"></label>
+                          <input id="ck2" type="checkbox" checked/>
+
+              <label for="ck2"></label>
+                         <input id="ck3" type="checkbox" />
+
+              <label for="ck3"></label>
+                           <input id="ck4" type="checkbox" />
+            <label for="ck4"></label>
+                          <input id="ck5" type="checkbox" />
+              <label for="ck5"></label>
+
+           </div>
+         </div>
+         
+       </div>
+     <div class="section">
+     <div class="section__title">
+       Interests
+       </div>
+       <div class="section__list">
+         <div class="section__list-item">
+                  Ski, programming.
+          </div>
+       </div>
+     </div>
+     </div>
+  </div>
+</div>
+</body>
+</html>
